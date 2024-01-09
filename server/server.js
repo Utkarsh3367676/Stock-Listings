be2840dc7e9fdc7ce1eb1df777fdc5401bec5d33
Utkarsh Parihar <87323724+Utkarsh3367676@users.noreply.github.com>
2024-01-09 23:15:23 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -16,6 +16,14 @@ app.use(cors(
   }
 ));
 
+ app.use(function (req, res, next) {
+    //Enabling CORS
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, 
+    Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+      next();
+    });
 
 const polygonApiKey = 'QlOE7feVp6mhdVa4sovHtWyAhpB3BIAb';
 const stocksFile = path.join(__dirname, 'stocks.json');
