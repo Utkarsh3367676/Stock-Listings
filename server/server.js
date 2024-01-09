6bfc5847bbafeb2437b491c0ae02df3c26c76b07
Utkarsh Parihar <87323724+Utkarsh3367676@users.noreply.github.com>
2024-01-09 22:50:02 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors(
   {
-    origin: [],
+    origin: [https://stock-listings-frontend.vercel.app/],
     methods:["POST","GET"],
     credentials:true
   }
